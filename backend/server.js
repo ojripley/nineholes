af45514f94d6bf294de5b14761c15ce65603651d
ojripley <ojripley19@gmail.com>
2020-12-10 14:00:39 -0500

    server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-// server
+// load .env data into process.env
+require('dotenv').config();
+
+// server config
+const PORT = process.env.PORT || 8080;
+
+const express = require("express");
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const app = express();
+const server = require('http').Server(app);
+app.use(cors);
+app.use(bodyParser.urlencoded({ extended: true }));
+server.listen(PORT, () => {
+  console.log(`server listening on port ${8080}`);
+});
